e05b9c1c2bbf5df6b8d3d33dfbe4ba0391165da3
Johann Richard <189003+johannrichard@users.noreply.github.com>
2026-05-30 09:13:18 +0200

    test: 🧪 remove jest-based test infrastructure and fixtures

--- jest.setup.js
+++
@@ -1,4 +0,0 @@
-const moment = require("moment");
-
-// Obsidian exposes moment globally on window; replicate that for the node test environment
-global.window = Object.assign(global.window ?? {}, { moment });
